601487652078cdcb25e9fc2a7792d3ad30f46701
uptimiseit <saurabh@uptimiseit.com>
2025-12-17 16:47:29 +0530

    addd

--- app/components/HomePage/Blog.tsx
+++ app/components/HomePage/Blog.tsx
@@ -13,7 +13,7 @@ const blogPosts = [
     excerpt: "Exploring the cyclical nature of design trends and how depth is returning to modern interfaces.",
     date: "Oct 12, 2025",
     readTime: "5 min read",
-    image: "https://images.unsplash.com/photo-1558655146-d09347e0b7a9?auto=format&fit=crop&q=80&w=800",
+    image: "https://plus.unsplash.com/premium_photo-1661382011487-cd3d6b1d9dff?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     slug: "#"
   },
   {
