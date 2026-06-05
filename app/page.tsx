d7e711bbb472300c09c8d3bff9578b3ca73df311
uptimiseit <saurabh@uptimiseit.com>
2026-06-05 14:08:43 +0530

    cc

--- app/page.tsx
+++ app/page.tsx
@@ -83,7 +83,7 @@ export default function Home() {
       {/* <IndustriesSection /> */}
       {/* <TransparencySection /> */}
       {/* <EnterpriseStandards /> */}
-      <ProjectsLedger />
+      {/* <ProjectsLedger /> */}
       {/* <ProjectEstimator /> */}
       {/* <NewBlogs /> */}
       {/* <TechTrends /> */}
